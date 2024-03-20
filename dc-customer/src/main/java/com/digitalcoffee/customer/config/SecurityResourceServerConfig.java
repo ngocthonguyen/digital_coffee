bacf18da798acc144fd999d9d96f8c658b0fa0c1
Ngoc Tho NGUYEN <ngoctho.nguyen@gmail.com>
2024-03-20 23:39:07 +0700

    Documentation

--- dc-customer/src/main/java/com/digitalcoffee/customer/config/SecurityResourceServerConfig.java
+++ dc-customer/src/main/java/com/digitalcoffee/customer/config/SecurityResourceServerConfig.java
@@ -15,11 +15,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
-@EnableMethodSecurity
+//@EnableMethodSecurity
 public class SecurityResourceServerConfig{
 
 
-    public static final String[] PUBLIC_URLS = {"/actuator/health", "/swagger-ui/*", "/v3/api-docs/*", "/v3/api-docs", "/customers"};
+    public static final String[] PUBLIC_URLS = {"/actuator/health", "/swagger-ui/*",
+            "/v3/api-docs/*", "/v3/api-docs", "/customers?username=*"};
     @Value("${jwt.signerKey}")
     private String signerKey;
 
@@ -28,7 +29,7 @@ public class SecurityResourceServerConfig{
         http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(PUBLIC_URLS).permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
         );
